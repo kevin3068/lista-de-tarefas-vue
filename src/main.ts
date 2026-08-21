d7a1513b33c3fc10db6ac80d43c4aefe411c7a02
kevin3068 <268755134+kevin3068@users.noreply.github.com>
2026-08-21 20:31:08 -0300

    1.1.2 - Bug Fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.config.devtools = false
-app.mount('#app')
+createApp(App).mount('#app')
